065d600ab05d7c5df736820c8812baab3cc7dbc7
Ayden Theodore <43591555+AydenTheodore@users.noreply.github.com>
2022-06-06 20:27:58 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,9 @@
+using System; // Allows you to use the System namespace
+
+class Program
+{
+    public static void Main(string[] args)
+    {	
+        Console.WriteLine("Hello World"); // You can also use Write() for writing on the same line!
+    }
+}
